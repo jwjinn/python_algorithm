15be4836aa41d769b1a35b0b88cb690aac8e3471
jwjinn <jwjinn@gmail.com>
2023-12-07 15:27:27 +0900

    first commit

---
+++ 색션0/변수입력과 연산자.py
@@ -0,0 +1,41 @@
+
+
+
+# a = input("숫자를 입력하세요: ")
+
+# print(a)
+
+
+# a, b = input("숫자를 입력하세요: ").split()
+# print(a+b)
+
+# a, b = input("숫자를 입력하세요: ").split()
+# a = int(a)
+# b = int(b)
+# print(a+b)
+
+# a, b = map(int, input("숫자를 입력하세요: ").split())
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a % b)
+# print(a ** 3)
+
+# a = 4.3
+# b = 5
+# c = a +b
+
+# print(type(a + b+ c))
+
+
+a = input("숫자를 입력하세요: ")
+print(a)
+print(type(a))
+
+print("====================")
+
+after_split = a.split()
+print(after_split)
+print(type(after_split))
