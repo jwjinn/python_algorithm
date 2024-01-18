ac1faf54c1ad17c51d84e1adcc80c058c5e9db12
jwjinn <jwjinn@gmail.com>
2024-01-18 16:07:51 +0900

    2024-01-18 두문제 commit

---
+++ 색션5/아나그램/AA.py
@@ -0,0 +1,53 @@
+"""
+아나그램
+
+순서가 다르더라도 알파펫과 그 갯수가 일치해서 배열해서 상대편의 단어가 될 수 있는
+것을 아나그램이라고 한다.
+
+
+
+"""
+import sys
+# sys.stdin=open("input.txt", "rt")
+
+# n1 = input()
+
+n1 = list(map(str, input()))
+# print(n1)
+
+word1 = dict()
+
+for i in n1:
+
+    if word1.get(i):
+        word1[i] +=1
+
+    else:
+        word1[i] =1
+
+word1 = sorted(word1.items(), key= lambda x: x[0])
+# 모든 iterator를 받는 sorted 매서드.. 그리고 애초에 정렬해서 갑을 집어넣는 것이 아니라면 딕셔너리 형태에서의 key값을 기준으로 정렬되는 딕셔너리는 없다.
+# items 매서드를 이용해서 튜플로 만들고 람다식을 써야 할 듯.
+
+# print(word1)
+
+n2 = list(map(str, input()))
+# print(n1)
+
+word2 = dict()
+
+for i in n2:
+
+    if word2.get(i):
+        word2[i] +=1
+
+    else:
+        word2[i] =1
+
+word2 = sorted(word2.items(), key= lambda x: x[0])
+# sorted(word2, key= lambda x: x[0])
+# sorted(word2)
+# print(word2)
+
+if word1 == word2:
+    print("YES")
---
+++ 색션5/최대힙/최대힙.py
@@ -0,0 +1,34 @@
+"""
+최소힙
+
+구성은 부모 노드값이 왼쪽 자식과 오른 자식노드의 값보다 작게 트리를 구성해야 한다.
+
+부모가 왼쪽, 오른쪽 자식노드보다 작아야 한다.
+
+내가 풀지 못한 이유는 노드의 원리를 알지 못했던 점, 파이선에 관련 매서드가
+있다는 점을 몰랐다는 점.
+
+
+"""
+
+import sys
+import heapq as hq
+
+sys.stdin=open("input.txt", "rt")
+a = []
+
+while True:
+    n = int(input())
+
+    if n == -1:
+        break
+
+    if n == 0:
+        if len(a) ==0:
+            print(-1)
+
+        else:
+            print(-hq.heappop(a))
+
+    else:
+        hq.heappush(a, -n)
